a3473d57221750b7ad12a4d94f1204b5a6db387a
Nozomi Shibano <shibano.nozomi@gmail.com>
2018-10-15 14:09:44 +0900

    clean

--- Malmacs/Program.fs
+++ Malmacs/Program.fs
@@ -9,14 +9,6 @@ open System.Reflection
 [<DllImport("user32.dll")>]
 extern bool SetProcessDPIAware()
 
-let readJson path name =
-    try
-        let s = System.IO.File.ReadAllText(path)
-        MalJson.parse(s)
-    with exn ->
-        Debug.WriteLine(sprintf "%s json read failed: %s" name exn.Message)
-        MalJson.json.Jobject [||]
-
 [<EntryPoint; STAThread>]
 let main args = 
     if Environment.OSVersion.Version.Major >= 6 then SetProcessDPIAware() |> ignore
@@ -39,21 +31,12 @@ let main args =
                 | Some h -> Path.GetFileName (h.OriginalPath)
             let text = Doc.getAllString editor.Doc
             File.WriteAllText(Path.Combine(logDir, id + name), text))
-        
-    //let configJsonPath = Path.Combine(exeDir, "config.json")
-    //let configJson = readJson "Config" configJsonPath
-
-    //let persistentJsonPath = Path.Combine(exeDir, "persistent.json")
-    //let persistentJson = readJson "Persistent" persistentJsonPath
-    //let persistentJsonOfs = repl.Mal.Var("persistent_json", persistentJson)
 
     Application.Idle.Add(fun ev ->
         if repl.Editors.Count = 0 then
             Application.Exit()
         let mutable m = Win32.Message()
         while (not (Win32.PeekMessage(&m, IntPtr.Zero, 0u, 0u, 0u))) && repl.Tick() do ())
-    
-
 
     if args.Length > 0 then
         repl.OpenPathWithNewEditor(args.[0])
@@ -62,16 +45,4 @@ let main args =
 
     Application.Run()
 
-    //let json =
-    //    try repl.Mal.GetFromOfs(persistentJsonOfs)
-    //    with _ -> MalJson.json.Jobject [||]
-    //let json =
-    //    match json with
-    //    | MalJson.json.Jobject _
-    //    | MalJson.json.Jarray _ -> json
-    //    | _ -> MalJson.json.Jobject [|("root_atom_backup", json)|]
-    //try
-    //    File.WriteAllText(persistentJsonPath, json.ToString(false))
-    //with exn ->
-    //    Debug.WriteLine(sprintf "Persistent json write failed: %s" exn.Message)
     0
