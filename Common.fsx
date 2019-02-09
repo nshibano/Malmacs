3dd4b1d22092e7c809436f38f24fb85a999218f4
Nozomi Shibano <shibano.nozomi@gmail.com>
2019-02-09 18:44:07 +0900

    Publish.fsx

---
+++ Common.fsx
@@ -0,0 +1,38 @@
+﻿open System
+open System.Diagnostics
+open System.IO
+
+let cmd name arg =
+    Console.WriteLine(name + " " + arg)
+    let info = ProcessStartInfo(name, arg)
+    info.RedirectStandardOutput <- true
+    info.RedirectStandardError <- true
+    info.UseShellExecute <- false
+    info.CreateNoWindow <- true
+    let proc = Process.Start(info)
+    proc.OutputDataReceived.Add(fun x -> Console.WriteLine(x.Data))
+    proc.ErrorDataReceived.Add(fun x -> Console.Error.WriteLine(x.Data))
+    proc.BeginOutputReadLine()
+    proc.BeginErrorReadLine()
+    proc.WaitForExit()
+    if proc.ExitCode <> 0 then failwithf "command %s %s failed with exit code %d." name arg proc.ExitCode
+
+let ensureDirectory (pathToDir : string) =
+    try Directory.CreateDirectory(pathToDir) |> ignore
+    with _ -> ()
+    
+let copyFile pathFrom pathTo =
+    ensureDirectory (Path.GetDirectoryName(pathTo))
+    File.Copy(pathFrom, pathTo, true)
+
+let copyDir origDir newDir =
+    Directory.CreateDirectory(newDir) |> ignore
+    for file in Directory.GetFiles(origDir) do
+        File.Copy(file, Path.Combine(newDir, Path.GetFileName(file)))
+
+let copyAllFiles fromDir toDir =
+    for file in Directory.GetFiles(fromDir) do
+        let toPath = Path.Combine(toDir, Path.GetFileName(file))
+        ensureDirectory (Path.GetDirectoryName(toPath))
+        File.Copy(file, Path.Combine(toDir, Path.GetFileName(file)), true)
+    
---
+++ Publish.fsx
@@ -0,0 +1,35 @@
+﻿#load "Common.fsx"
+#r @"System.IO.Compression.dll"
+#r @"System.IO.Compression.FileSystem.dll"
+
+open System
+open System.IO
+open System.IO.Compression
+
+open Common
+
+let inAppveyor, buildVersion =
+    let s = Environment.GetEnvironmentVariable("APPVEYOR_BUILD_VERSION")
+    if isNull s then
+        false, "0.0.0"
+    else
+        true, s
+
+let pushArtifact path =
+    if inAppveyor then
+        cmd "appveyor" ("PushArtifact " + path)
+
+if Directory.Exists("Publish") then
+    Directory.Delete("Publish", true)
+Directory.CreateDirectory(@"Publish") |> ignore
+cmd "msbuild" @"/p:Configuration=Release Malmacs.sln"
+
+
+copyAllFiles @"Malmacs\bin\Release" "Publish"
+copyAllFiles @"Malmacs\Mal" @"Publish\Mal"
+copyFile @"Malmacs\Malmacs.config.Release.json" @"Publish\Malmacs.config.json"
+let net45ZipFileName = sprintf "Malmacs-%s.zip" buildVersion
+if File.Exists(net45ZipFileName) then
+    File.Delete(net45ZipFileName)
+ZipFile.CreateFromDirectory("Publish", net45ZipFileName)
+pushArtifact net45ZipFileName
